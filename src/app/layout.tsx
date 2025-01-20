dfc21a8aee995d6daf33c493e377f7cf59ebd72c
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-01-20 18:34:34 +0100

    integrated the root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,21 @@
-import "./global.css";
+import "./global.scss";
 import React from "react";
-import {} from "next";
+import type { Metadata } from "next";
+import { catFaceImg } from "@/constants/media";
+import HistoryProvider from "@/providers/history.provider";
+
+export const metadata: Metadata = {
+  icons: catFaceImg.src,
+  title: "Cat Mood Tracker",
+  description: "Track your cat's mood",
+};
+
+export default function Rootlayout(properties: React.PropsWithChildren) {
+  return (
+    <html lang="en">
+      <HistoryProvider>
+        <body>{properties.children}</body>
+      </HistoryProvider>
+    </html>
+  );
+}

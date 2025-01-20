e23045b6bc3be6e59a7f92e4de50607e2252948e
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-01-20 18:35:29 +0100

    created the home page and imported the left and right section components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,12 @@
-export default function Page(){
-return <></>;
+import "./style.scss";
+import Left from "./(components)/left";
+import Right from "./(components)/right";
+
+export default function Page() {
+  return (
+    <section className="home">
+      <Left />
+      <Right />
+    </section>
+  );
 }
